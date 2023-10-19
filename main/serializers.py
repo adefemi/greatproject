2ad86d213eddcfed9299409c9651dac43f180403
Adefemi Oseni <adefemigreat1995@gmail.com>
2023-10-19 12:28:07 +0100

    updated

--- main/serializers.py
+++ main/serializers.py
@@ -3,10 +3,13 @@ from .models import (ProjectModel, SubmissionModel)
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+	submission_count = serializers.SerializerMethodField()
 	class Meta:
 		model = ProjectModel
 		fields = '__all__'
 
+	def get_submission_count(self, obj):
+		return SubmissionModel.objects.filter(project=obj).count()
 
 class SubmissionSerializer(serializers.ModelSerializer):
 	project = ProjectSerializer(read_only=True)
